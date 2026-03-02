cf1364000a0d5f439b3732176d71cfc8046403a2
Pattharaphon <crca4577@gmail.com>
2026-03-02 19:52:35 +0700

    add

--- pokemon/core/route.py
+++ pokemon/core/route.py
@@ -12,9 +12,10 @@ def index():
                          title='Home Page',
                          pokemons=pokemons)
 
-@core_bp.route('/<int:id>/detail')
-def detail(id):
-  pokemon = db.session.get(Pokemon, id)
-  return render_template('core/pokemon_detail.html',
-                         title='Pokemon Detail Page',
-                         pokemon=pokemon)
+@core_bp.route('/')
+def index():
+  page = request.args.get('page', type=int)    # ให้เพิ่ม type=int 
+  pokemons = db.paginate(sa.select(Pokemon), per_page=4, page=page)
+  return render_template('core/index.html',
+                         title='Home Page',
+                         pokemons=pokemons)
