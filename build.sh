962439abe78ce9363e1dfebd925377db1f59e34a
Pattharaphon <crca4577@gmail.com>
2026-03-02 22:43:02 +0700

    debug: print env var

--- build.sh
+++ build.sh
@@ -4,9 +4,6 @@ set -o errexit
 pip install -r requirements.txt
 
 python -c "
-from main import app
-from pokemon.extensions import db
-with app.app_context():
-    db.create_all()
-    print('Tables created successfully!')
+import os
+print('DATABASE_URL =', os.environ.get('DATABASE_URL', 'NOT FOUND'))
 "
